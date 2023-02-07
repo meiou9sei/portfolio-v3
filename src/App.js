072d583a14eb78dac5b42e5110e6e761de6951d1
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-02-07 13:06:25 -0500

    🍱 set initial content "hello world"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'>hello world</div>;
 }
 
 export default App;
